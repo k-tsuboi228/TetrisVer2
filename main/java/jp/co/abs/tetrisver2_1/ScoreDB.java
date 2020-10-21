940c6ae237906339ad9aec07969df75e115b8a63
k-tsuboi228 <73216879+k-tsuboi228@users.noreply.github.com>
2020-10-21 17:10:26 +0900

    Add files via upload

---
+++ main/java/jp/co/abs/tetrisver2_1/ScoreDB.java
@@ -0,0 +1,59 @@
+
+package jp.co.abs.tetrisver2_1;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class ScoreDB extends SQLiteOpenHelper{
+
+    private static final int DATABASE_VERSION = 3;
+
+    private static final String DATABASE_NAME = "ScoreDB.db";
+    private static final String TABLE_NAME = "scoreDB";
+    private static final String _ID ="_id";
+    private static final String COLUMN_NAME_TITLE = "score";
+    private static final String COLUMN_NAME_SUBTITLE = "line";
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    _ID + " INTEGER PRIMARY KEY," +
+                    COLUMN_NAME_TITLE + " TEXT," +
+                    COLUMN_NAME_SUBTITLE + " INTEGER)";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + TABLE_NAME;
+
+    ScoreDB(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL(
+                SQL_CREATE_ENTRIES
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(
+                SQL_DELETE_ENTRIES
+        );
+        onCreate(db);
+    }
+
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+/*
+    public void saveData(SQLiteDatabase db, int score, int line){
+        ContentValues values = new ContentValues();
+        values.put("score", score);
+        values.put("line", line);
+
+        db.insert("scoreDB", null, values);
+    }
+*/
+}
---
+++ main/java/jp/co/abs/tetrisver2_1/StartActivity.java
@@ -0,0 +1,88 @@
+package jp.co.abs.tetrisver2_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class StartActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
+
+     private ScoreDB scoredb;
+     private TextView highScoreView;
+     Switch BGMSwitch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+//DB作成
+        scoredb = new ScoreDB(getApplicationContext());
+
+        highScoreView = findViewById(R.id.highScore);
+
+        SQLiteDatabase db = scoredb.getReadableDatabase();
+
+        String order_by = "score DESC";
+
+        Cursor cursor = db.query(
+                "scoreDB",
+                new String[]{"score", "line"},
+                null,
+                null,
+                null,
+                null,
+                order_by
+                );
+
+            boolean mov = cursor.moveToFirst();
+
+            StringBuilder sbuilder = new StringBuilder();
+
+            if(mov == true) {
+                for (int i = 0; i < 3; i++) {
+                    sbuilder.append("Score: ");
+                    sbuilder.append(cursor.getInt(0));
+                    sbuilder.append("\t" + "Line: ");
+                    sbuilder.append(cursor.getInt(1) + "\n");
+                    cursor.moveToNext();
+                }
+            }
+
+            cursor.close();
+
+            highScoreView.setText(sbuilder.toString());
+            BGMSwitch = findViewById(R.id.BGMSwitch);
+
+            BGMSwitch.setOnCheckedChangeListener(this);
+    }
+
+    public void onClick(View view){
+        Intent intent = new Intent(this,TetrisActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+
+        SharedPreferences Count = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor Editor = Count.edit();
+
+        if(isChecked == true) {
+            Editor.putBoolean("Switch", true);
+            Editor.commit();
+        }else{
+            Editor.putBoolean("Switch", false);
+            Editor.commit();
+        }
+
+
+    }
+}
